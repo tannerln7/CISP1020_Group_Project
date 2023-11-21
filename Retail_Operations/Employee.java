5407d7300f007de60c27f344fbe05231dd1de515
dmndwarrior873 <129331621+dmndwarrior873@users.noreply.github.com>
2023-11-21 15:28:34 -0500

    Update Employee.java

--- Retail_Operations/Employee.java
+++ Retail_Operations/Employee.java
@@ -3,7 +3,38 @@ package Retail_Operations;
 import Helpers.JsonIdentifiable;
 //TODO: Start working on the Management menu for Employees.
 //BODy: This will be a menu that allows staff members to add, remove, and edit employee information.
-public record Employee(String name, String employeeID, String position, String username, String password) implements JsonIdentifiable {
+public class Employee implements JsonIdentifiable {
+    private String name;
+    private String employeeID;
+    private String position;
+    private String username;
+    private String password;
+
+    public Employee(String name, String employeeID, String position, String username, String password) {
+        this.name = name;
+        this.employeeID = employeeID;
+        this.position = position;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    
 
     private String getEmployeeID() {
         return employeeID;
@@ -14,3 +45,4 @@ public record Employee(String name, String employeeID, String position, String u
     }
 
 }
+
