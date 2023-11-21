f582eccf889d741b065073c497f9cd25ab92e15a
dmndwarrior873 <129331621+dmndwarrior873@users.noreply.github.com>
2023-11-21 16:39:09 -0500

    added documentation to Employee.java

--- Retail_Operations/Employee.java
+++ Retail_Operations/Employee.java
@@ -10,6 +10,14 @@ public class Employee implements JsonIdentifiable {
     private String username;
     private String password;
 
+    /**
+     * the constructor with five parameters
+     * @param name the name of the employee
+     * @param employeeID the ID of the employee
+     * @param position the position of the employee
+     * @param username the username of the employee
+     * @param password the password of the employee
+     */
     public Employee(String name, String employeeID, String position, String username, String password) {
         this.name = name;
         this.employeeID = employeeID;
@@ -18,24 +26,44 @@ public class Employee implements JsonIdentifiable {
         this.password = password;
     }
 
+    /**
+     * gets the name of the employee
+     * @return the name of the employee
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * gets the position of the employee
+     * @return the position of the employee
+     */
     public String getPosition() {
         return position;
     }
 
+    /**
+     * gets the username of the employee
+     * @return the username of the employee
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * gets the password of the employee
+     * @return the password of the employee
+     */
     public String getPassword() {
         return password;
     }
 
     
 
+    /**
+     * gets the ID of the employee
+     * @return the ID of the employee
+     */
     private String getEmployeeID() {
         return employeeID;
     }
@@ -43,6 +71,8 @@ public class Employee implements JsonIdentifiable {
     public String getJsonId() {
         return "Employee_" + this.getEmployeeID();
     }
+
+}
 
 }
 
