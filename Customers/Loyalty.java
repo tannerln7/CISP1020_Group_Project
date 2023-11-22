5d0d2247c398312339cfbdafa1fe5c04cc0d52f6
Juneve <149718182+Juneve@users.noreply.github.com>
2023-11-21 22:09:15 -0500

    Update Loyalty.java
    Added/reworded comments.

--- Customers/Loyalty.java
+++ Customers/Loyalty.java
@@ -2,22 +2,24 @@ package Customers;
 
 import Products.Discount;
 
-/**
- *
- * @author Donnie Young
- */
-
+// The Loyalty class manages a customer's loyalty points and rewards discounts.
 public class Loyalty {
 
+    // Represents the total accumulated loyalty points for the customer.
     private double points;
+
+    // The conversion rate between spending and points earned.
     private final double pointRatio = 0.1;
+
+    // The discount applied to purchases when redeeming points.
     private Discount rewardsDiscount = new Discount(0.05);
 
-    //sets points to sign up bonus starting with 10000 points.
+    //Constructor that initializes the loyalty account with a sign-up bonus of 10,000 points.
     public Loyalty() {
         this.points = 10000;
     }
-    //Used to create a LoyaltyAccount object with custom parameters
+
+    // Constructor that allows customizing the initial points and rewards discount.
     public Loyalty(double initialPoints, Discount rewardsDiscount) {
         this.points = initialPoints;
         this.rewardsDiscount = rewardsDiscount;
@@ -28,21 +30,25 @@ public class Loyalty {
         this.points += amount;
     }
 
+    // Constructor that allows customizing the initial points and rewards discount.
     public double getPoints() {
         return this.points;
     }
 
+    // Checks if the customer has enough points to redeem the specified discount.
     public boolean hasEnoughPointsForDiscount(double pointsRedeemed) {
         return this.points >= pointsRedeemed * this.rewardsDiscount.getDiscountPercent();
     }
 
-
+    // Redeems the specified amount of points, deducting them from the customer's balance.
     public void redeemPoints(double points) {
         if (hasEnoughPointsForDiscount(points)) {
             this.points -= points;
         }
     }
+    
     //1 point per 10 dollars spent. Points are worth 10 cents each. 100 points = $10.00
+    // Calculates the discount based on the points redeemed and the rewards discount.
     public Discount calculateDiscount(double pointsRedeemed) {
         if (hasEnoughPointsForDiscount(pointsRedeemed)) {
             return new Discount(this.rewardsDiscount.getDiscountPercent(), pointsRedeemed * pointRatio);
@@ -50,10 +56,14 @@ public class Loyalty {
             return new Discount(this.rewardsDiscount.getDiscountPercent());
         }
     }
+
+    // Retrieves the percentage value of the rewards discount.
     public double getRewardsDiscountPercent(){
         return this.rewardsDiscount.getDiscountPercent();
     }
 }
+
+
 //   Following code is meant to give an idea of what I was thinking would call or use loyalty class
 
 //public float calculateTotalPrice(List<Item> items, LoyaltyProgram loyaltyProgram) {
