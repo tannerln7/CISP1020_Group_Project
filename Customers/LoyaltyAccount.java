a9de33ce139af7dfc11fcaac018022c25b113d75
Juneve <149718182+Juneve@users.noreply.github.com>
2023-11-21 22:17:57 -0500

    Update LoyaltyAccount.java
    Added comments.

--- Customers/LoyaltyAccount.java
+++ Customers/LoyaltyAccount.java
@@ -1,9 +1,13 @@
 package Customers;
 import Products.Discount;
 
+// The LoyaltyAccount class extends the Loyalty class to represent a customer's loyalty account, including an account number.
 public class LoyaltyAccount extends Loyalty{
 
+    // The unique account number associated with the customer.
     private final int accountNumber;
+
+    // A counter to generate unique account numbers.
     private static int lastAccountNumber = 100000;
 
     //Used as a default constructor. Uses default pointsBalance and creates a default rewardsDiscount object
@@ -13,13 +17,14 @@ public class LoyaltyAccount extends Loyalty{
         lastAccountNumber++;
     }
 
-    //Used to create a LoyaltyAccount object with custom parameters
+    // Constructor that allows customizing the initial points balance and rewards discount.
     public LoyaltyAccount(double initialPointsBalance, Discount rewardsDiscount) {
         super(initialPointsBalance, rewardsDiscount);
         this.accountNumber = lastAccountNumber++;
         lastAccountNumber++;
     }
 
+    // Retrieves the customer's account number.
     public double getAccountNumber() {
         return this.accountNumber;
     }
