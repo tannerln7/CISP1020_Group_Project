ef6a6cad42eec60443bad4bac8ae5726feec1f11
Juneve <149718182+Juneve@users.noreply.github.com>
2023-11-21 21:57:08 -0500

    Update Customer.java
    Added comments to all bits of code in Customer. Feel free to add more if you think I missed something.

--- Customers/Customer.java
+++ Customers/Customer.java
@@ -10,6 +10,12 @@ import java.util.ArrayList;
 
 //TODO: Start working on the user menu for Customers.
 //BODy: This will be a menu that allows customers to view their information, update their information, view their receipts, and sign up for a rewards account.
+
+//Added commens
+/**
+ * This class represents a customer of the store. It stores the customer's name, phone number,
+ * and a list of their receipts.
+ */
 public class Customer implements JsonIdentifiable{
     private final int customerId;
     private String name;
@@ -45,29 +51,56 @@ public class Customer implements JsonIdentifiable{
         return this.phoneNumber;
     }
 
-    //Change Phone Number method
+    /**
+    * Changes the customer's phone number.
+    *
+    * @param newPhoneNumber The new phone number for the customer.
+    */
     public void changePhoneNumber(String newPhoneNumber) {
         this.phoneNumber = newPhoneNumber;
     }
-
+    
+    /**
+    * Adds a receipt to the customer's list of receipts.
+    *
+    * @param receipt The receipt to add.
+    */
     public void addReceipt(Receipt receipt){
         receipts.add(receipt);
     }
 
+    /**
+    * Retrieves the customer's list of receipts.
+    *
+    * @return The customer's list of receipts.
+    */
     public ArrayList<Receipt> getReceipts(){
         return this.receipts;
     }
 
+    /**
+    * Updates the customer's save file by converting the customer object to JSON format and writing it to a file.
+    */
     public void updateSaveFile(){
         ObjectJson.objectToJson(this);
     }
 
+    /**
+    * Generates a unique customer ID by incrementing the `lastCustomerId` variable and returning the new value.
+    *
+    * @return The newly generated customer ID.
+    */
     private int generateId() {
         this.lastCustomerId++;
         return lastCustomerId;
     }
 
 
+    /**
+    * Overrides the default `toString()` method to provide a human-readable representation of the customer object.
+    *
+    * @return A string representation of the customer, including their customer ID, name, and phone number.
+    */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -79,6 +112,11 @@ public class Customer implements JsonIdentifiable{
         return sb.toString();
     }
 
+    /**
+    * Overrides the `getJsonId()` method to generate a JSON-compatible ID for the customer.
+    *
+    * @return The JSON-compatible ID for the customer, which is formed by prefixing the customer ID with "Customer_".
+    */
     @Override
     public String getJsonId() {
         return "Customer_" + this.customerId;
